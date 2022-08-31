3409de91f63eee1aaf49449c349ee2e1eb7fc029
SimpleSanyaCode <SimpleSanyaCode@internet.ru>
2022-08-31 14:33:40 +0300

    fixet bat / add enter name and scanf / 00001

--- src/hello.go
+++ src/hello.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	var line string
 	fmt.Println("Hello world , go!")
+	fmt.Println("What's your name?")
+	fmt.Scanf("%s\n", &line)
+	fmt.Printf("Привет , %s!\n", line)
 }
